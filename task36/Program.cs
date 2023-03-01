ea8c415916f6f5ef1100ee5776bfb279291afc15
Dmitriy Romanyuk <dmitriy.romanyuk1@gmail.com>
2023-03-01 16:12:25 +0300

    sem_5_dz_final

---
+++ task36/Program.cs
@@ -0,0 +1,49 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов,
+// стоящих на нечётных позициях.
+
+// [3, 7, 23, 12] -> 19
+
+// [-4, -6, 89, 6] -> 0
+
+int Promt (string message)
+{
+  System.Console.WriteLine(message);
+  string str = System.Console.ReadLine() ?? String.Empty;
+  double numberDouble = (int) Convert.ToDouble(str);
+  int result = (int) Math.Abs(Convert.ToInt32(numberDouble));
+  if (result == 0)
+  {
+    System.Console.WriteLine("Указан недостаточный размер массива");
+    return -1;
+  }
+  return result;
+}
+
+int [] FillDigitPositiveArray (int sizeArray)
+{
+  int [] fillArray = new int [sizeArray];
+  Random rnd = new Random();
+  for (int i = 0; i < sizeArray; i++)
+  {
+    fillArray[i] = rnd.Next(-9, 10);
+  }
+  return fillArray;
+}
+
+
+int SumOfElementsOfOddIndeces (int [] ArrayIn) 
+{
+  int sum = 0;
+  for (int i = 1; i < ArrayIn.Length; i+=2) 
+  {
+    sum += ArrayIn[i];
+  }
+  return sum;
+}
+
+int sizeArray = Promt ("Введите размер массива");
+int [] newArray = FillDigitPositiveArray(sizeArray);
+System.Console.WriteLine(string.Join("; ", newArray));
+System.Console.WriteLine($"Сумма элементов нечетных чисел равна {SumOfElementsOfOddIndeces(newArray)}");
+
+
---
+++ task38/Program.cs
@@ -0,0 +1,65 @@
+﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементом массива.
+
+// [3 7 22 2 78] -> 76
+
+
+int Promt (string message)
+{
+  System.Console.WriteLine(message);
+  string str = System.Console.ReadLine() ?? String.Empty;
+  double numberDouble = (int) Convert.ToDouble(str);
+  int result = (int) Math.Abs(Convert.ToInt32(numberDouble));
+  if (result == 0)
+  {
+    System.Console.WriteLine("Указан недостаточный размер массива");
+    return -1;
+  }
+  return result;
+}
+
+ 
+
+double [] PaddingWithRealNumbers (int sizeArray)
+{
+  double [] fillArray = new double [sizeArray];
+  Random rnd = new Random();
+  for (int i = 0; i < sizeArray; i++)
+  {
+    fillArray[i] = rnd.Next(-99,100) + Math.Round(rnd.NextDouble(), 2);
+  }
+  return fillArray;
+}
+
+
+
+double DifferenceMaxMinValue (double [] ArrayIn)
+{
+  double maxElement = ArrayIn[0];
+  double minElement = ArrayIn[0];
+  if (ArrayIn.Length == 1)
+  {
+    System.Console.WriteLine("Недастаточно эллементов массива");
+    return -1;
+  }
+
+  for (int i = 0; i < ArrayIn.Length; i++)
+  {
+    if (maxElement<ArrayIn[i])
+    {
+      maxElement = ArrayIn[i];
+    }
+    if (minElement>ArrayIn[i])
+    {
+      minElement = ArrayIn[i];
+    }
+  }
+  System.Console.WriteLine($"Максимальное значение: {maxElement}");
+  System.Console.WriteLine($"Минимальное значение: {minElement}");
+  return maxElement-minElement;
+}
+
+
+int sizeDoubleArray = Promt ("Ввдите размер массива");
+double [] newArray = PaddingWithRealNumbers(sizeDoubleArray);
+System.Console.WriteLine(string.Join("; ", newArray));
+System.Console.WriteLine($"Разница между максимальным и минимальным элементом массива: {DifferenceMaxMinValue(newArray)}");
